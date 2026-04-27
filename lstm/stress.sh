5a22e81722c59b72aedf37982de28c6009ebb74d
Kass <jazzjunior1@gmail.com>
2026-04-27 12:11:47 +0100

    Updated stress.sh

--- lstm/stress.sh
+++ lstm/stress.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo "Starting stress test..."
 
-stress-ng --cpu 1 --cpu-load 20 --cpu-method matrixprod --vm 1 --vm-bytes 512M --timeout 300 &
+CORES=$(nproc)
+stress-ng --cpu $CORES --cpu-load 5 --cpu-method matrixprod --timeout 300 &
+sleep 30
+stress-ng --cpu $CORES --cpu-load 10 --cpu-method matrixprod --vm 1 --vm-bytes 512M --timeout 270 &
 sleep 20
-stress-ng --cpu 1 --cpu-load 20 --cpu-method matrixprod --vm 1 --vm-bytes 512M --timeout 280 &
+stress-ng --cpu $CORES --cpu-load 15 --cpu-method matrixprod --vm 1 --vm-bytes 512M --timeout 250 &
 sleep 20
-stress-ng --cpu 1 --cpu-load 20 --cpu-method matrixprod --vm 1 --vm-bytes 512M --timeout 260 &
+stress-ng --cpu $CORES --cpu-load 25 --cpu-method matrixprod --vm 1 --vm-bytes 1024M --timeout 230 &
 sleep 20
-stress-ng --cpu 1 --cpu-load 20 --cpu-method matrixprod --vm 1 --vm-bytes 512M --timeout 240
+stress-ng --cpu $CORES --cpu-load 30 --cpu-method matrixprod --vm 1 --vm-bytes 2048M --timeout 210
 
 echo "Stress test complete."
